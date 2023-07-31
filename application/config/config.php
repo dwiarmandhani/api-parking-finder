a9add44864df659866fedc23e1272f89e9d4477f
dwiarmandhani <60287812+dwiarmandhani@users.noreply.github.com>
2023-07-31 23:19:43 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/api-parking-finder/';
+$config['base_url'] = 'https://parking-finder-2ppw2g7m3-parkingapp.vercel.app';
 
 /*
 |--------------------------------------------------------------------------
